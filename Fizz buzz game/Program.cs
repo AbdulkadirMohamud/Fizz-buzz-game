8a158f7c3e0eef36cbc729403d0c094d1bec7a82
Abdulkadir <qansax30@hotmail.com>
2024-11-07 00:41:47 +0100

    Add project files.

---
+++ Fizz buzz game/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizz_buzz_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool threeDiv = false;
+            bool fiveDiv = false;
+
+            for (int i = 0; i <= 15; i++) //0-14-1-15
+            {
+                threeDiv = i % 3 == 0;
+                fiveDiv = i % 5 == 0;
+                {
+                    //9 % 3 = 0
+                    if ((i % 3 == 0 && i % 5 == 0) 
+                        {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+
+                    }
+
+
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                } }
+        }
+    }
+}
